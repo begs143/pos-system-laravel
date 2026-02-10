06bce350d8c69bdf4e2d1ad51605f9e36e3ceec0
MAKU <cfmark407@gmail.com>
2026-02-10 15:18:59 +0800

    update

--- database/migrations/2026_02_10_063017_add_batch_uuid_column_to_activity_log_table.php
+++ database/migrations/2026_02_10_063017_add_batch_uuid_column_to_activity_log_table.php
@@ -1,8 +1,14 @@
 <?php
 
+<<<<<<< Updated upstream
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+=======
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+>>>>>>> Stashed changes
 
 class AddBatchUuidColumnToActivityLogTable extends Migration
 {
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,10 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\InventoryController;
 use App\Http\Controllers\LogController;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
+=======
+>>>>>>> Stashed changes
 use App\Http\Controllers\ProfileController;
 >>>>>>> Stashed changes
 use App\Http\Controllers\PurchaseOrderController;
@@ -91,9 +94,12 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/logs', [LogController::class, 'index'])->name('admin.logs.index');
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     // Route Logs
     Route::get('/admin/logs', [LogController::class, 'index'])->name('admin.logs.index');
 
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 });
