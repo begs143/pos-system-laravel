f33d79b7cf91bf1191b3b375405f0ef889d6356d
MAKU <cfmark407@gmail.com>
2026-02-10 15:20:58 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ use App\Http\Controllers\LogController;
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -131,9 +134,12 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     // Route Logs
     Route::get('/admin/logs', [LogController::class, 'index'])->name('admin.logs.index');
 
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
